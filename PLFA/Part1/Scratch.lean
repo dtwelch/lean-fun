cbe0f6b958eefebcb28148b4b437b86ad2d897ff
dtwelch <dtw.welch@gmail.com>
2026-05-12 15:38:26 -0400

    more

--- PLFA/Part1/Scratch.lean
+++ PLFA/Part1/Scratch.lean
@@ -1,10 +1,15 @@
 import PLFA.Part1.Naturals
 
-theorem plus_assoc : ∀ (m n p : ℕ), (m + n) + p = m + (n + p) := by
-    intro m n p
-    induction m
-    | zero =>
-        calc
-            (.zero + n) + p = (n + p) := by rfl
-            _ = .zero + (n + p) := by rfl
-    | succ m ih => sorry
+-- theorem plus_assoc : ∀ (m n p : ℕ), (m + n) + p = m + (n + p) := by
+    -- intro m n p
+    -- induction m with
+    -- | zero =>
+        -- calc
+            -- (.zero + n) + p = (n + p)   := by rfl
+            -- _ = .zero + (n + p)         := by rfl
+    -- | suc m ih =>
+        -- calc
+            -- plus (plus (.suc m) n) p
+              -- = plus (.suc (plus m n)) p := by rfl
+            -- _ = .suc (plus (plus m n) p) := by rfl
+            -- _ =  := by sorry
---
+++ PLFA/aoc2015_1.lean
@@ -0,0 +1,45 @@
+-- day 1a
+inductive Paren where
+| lparen : Paren
+| rparen : Paren
+deriving Repr, DecidableEq
+
+def floor : List Paren -> Int := λ parens0 =>
+    let rec loop : List Paren -> Int -> Int :=
+        λ parens => λ floor =>
+            match parens with
+            | []            => floor
+            | .rparen :: ps => loop ps (floor - 1)
+            | .lparen :: ps => loop ps (floor + 1)
+    loop parens0 0
+
+-- day 1b:
+abbrev Pos : Type := Int
+
+def floor' : List Paren -> Int := λ parens0 =>
+    let rec loop : List Paren -> Int -> Pos -> Int :=
+        λ parens => λ floor => λ pos =>
+            match parens with
+            | []            => pos
+            | .rparen :: ps =>
+                if floor < 0 then pos else loop ps (floor - 1) pos + 1
+            | .lparen :: ps =>
+                if floor < 0 then pos else loop ps (floor + 1) pos + 1
+    loop parens0 0 1
+
+def readParen : Char -> (Except String Paren) :=
+    λ ch =>
+        match ch with
+        | '(' => Except.ok .lparen
+        | ')' => Except.ok .rparen
+        | e   => Except.error ("unexpected: " ++ String.singleton e)
+
+def read : String -> List Paren :=
+    λ input => String.foldr (λ ch acc =>
+        match readParen ch with
+         | Except.ok v      => v :: acc
+         | Except.error _   => acc
+        ) [] input
+
+#eval floor $ read "))((((("
+
